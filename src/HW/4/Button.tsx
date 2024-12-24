9d6da704f8a5d89ecc0f864142936be5c5a1fbcb
Rostislav Zhuk <zzzhuchok@gmail.com>
2024-12-24 12:09:16 +0300

    feat: add homework-01-04

--- src/HW/4/Button.tsx
+++ src/HW/4/Button.tsx
@@ -1,15 +1,15 @@
 type ButtonPropsType = {
-  callBack: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-  name: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+  callBack: () => void; // НУЖНО ПРОТИПИЗИРОВАТЬ
+  name: string; // НУЖНО ПРОТИПИЗИРОВАТЬ
 };
 
 export const Button = (props: ButtonPropsType) => {
   const callBackHandler = () => {
-    // НУЖНО ДОПИСАТЬ
+    props.callBack();
   };
 
   return (
-    <button id={'hw04-button'} onClick={callBackHandler}>
+    <button id={"hw04-button"} onClick={callBackHandler}>
       {props.name}
     </button>
   );
--- src/HW/4/HW4.tsx
+++ src/HW/4/HW4.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import s from './HW4.module.css';
-import { Button } from './Button';
-import { Input } from './Input';
+import { useState } from "react";
+import s from "./HW4.module.css";
+import { Button } from "./Button";
+import { Input } from "./Input";
 
 export const HW4 = () => {
   // 1️⃣ Раскомментируйте JSX (HW4.tsx) и вы увидите,
@@ -14,40 +14,39 @@ export const HW4 = () => {
 
   // ❗ Массив с данными не трогаем! Текст не меняем !
 
-  const [currentText, setCurrentText] = useState('');
+  const [currentText, setCurrentText] = useState("");
   const [texts, setTexts] = useState<string[]>([
-    'То, что вы делаете по ночам, то и делает вас богатым. (Аль Капоне)',
+    "То, что вы делаете по ночам, то и делает вас богатым. (Аль Капоне)",
   ]);
 
   const handleSave = () => {
     setTexts([currentText, ...texts]);
-    setCurrentText('');
+    setCurrentText("");
   };
 
   return (
-    <div id={'hw04'}>
-      {/*{currentText ? (*/}
-      {/*  <h1 id={'hw04-text'}>{currentText}</h1>*/}
-      {/*) : (*/}
-      {/*  <h1 id={'hw04-default-text'}>Здесь появится новое дело</h1>*/}
-      {/*)}*/}
+    <div id={"hw04"}>
+      {currentText ? (
+        <h1 id={"hw04-text"}>{currentText}</h1>
+      ) : (
+        <h1 id={"hw04-default-text"}>Здесь появится новое дело</h1>
+      )}
 
-      {/*<Input НЕ ХВАТАЕТ АРГУМЕНТОВ />*/}
+      <Input currentText={currentText} setCurrentText={setCurrentText} />
 
-      {/*<Button НЕ ХВАТАЕТ АРГУМЕНТОВ />*/}
+      {<Button callBack={handleSave} name="Save" />}
 
-      {/*<h1 style={{ marginTop: '50px' }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>*/}
+      <h1 style={{ marginTop: '50px' }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>
 
-      {/*<ol id={'hw04-tasks'}>*/}
-      {/*  {texts.map((el, index) => {*/}
-      {/*    return (*/}
-      {/*      <li key={index} id={`hw04-task-${index}`} className={ЕСЛИ ЧЕТН ? S.chetNechet : ''}>*/}
-      {/*        {el}*/}
-      {/*      </li>*/}
-      {/*    );*/}
-      {/*  })}*/}
-      {/*</ol>*/}
+      <ol id={'hw04-tasks'}>
+       {texts.map((el, index) => {
+         return (
+           <li key={index} id={`hw04-task-${index}`} className={index % 2 === 0 ? s.chetNechet : ''}>
+             {el}
+           </li>
+         );
+       })}
+      </ol>
     </div>
   );
 };
-
